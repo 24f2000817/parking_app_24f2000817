1a5c7d776fa7c6a9c7bdf492391ba408d4f90c91
24f2000817 <24f2000817@ds.study.iitm.ac.in>
2025-07-13 18:44:43 +0530

    Milestone-VP DB-Relationship

--- app.py
+++ app.py
@@ -1,1 +1,28 @@
-#Hello
+from flask import Flask
+from controllers.database import db, User, ParkingLot, ParkingSpot, Reservation
+from controllers.config import config
+
+app = Flask(__name__)
+app.config.from_object(config)
+db.init_app(app)
+
+with app.app_context():
+    db.create_all() # Create tables if they don't exist
+
+    Admin = User.query.filter_by(email = 'admin@gmail.com').first()
+    if not Admin:
+        Admin = User(
+            username='admin',
+            email='admin@gmail.com',
+            password='admin@123',
+            phone_number='1234567890'
+        )
+        db.session.add(Admin)
+    db.session.commit()
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ controllers/database.py
@@ -0,0 +1,41 @@
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+    email = db.Column(db.String(150), unique=True, nullable=False)
+    phone_number = db.Column(db.String(15), nullable=False)
+
+    reservations = db.relationship('Reservation', backref='user', lazy=True)
+
+class ParkingLot(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    prime_location_name = db.Column(db.String(100), nullable=False)
+    address = db.Column(db.String(200), nullable=False)
+    pin_code = db.Column(db.String(10), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    maximum_number_of_spots = db.Column(db.Integer, nullable=False)
+
+    spots = db.relationship('ParkingSpot', backref='lot', lazy=True)
+
+class ParkingSpot(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    lot_id = db.Column(db.Integer, db.ForeignKey('parking_lot.id'), nullable=False)
+    status = db.Column(db.String(1), default='A')  # A=Available, O=Occupied
+
+    reservation = db.relationship('Reservation', backref='spot', uselist=False)
+
+class Reservation(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    spot_id = db.Column(db.Integer, db.ForeignKey('parking_spot.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    vehicle_number = db.Column(db.String(20), nullable=False)
+    parking_timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+    leaving_timestamp = db.Column(db.DateTime, nullable=True)
+
+    cost_per_hour = db.Column(db.Float, nullable=False)
